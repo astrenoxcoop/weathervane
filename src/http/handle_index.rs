743f41cbd06f9e88a8ebf3e5c71150c9dda4eed5
Nick Gerakines <12125+ngerakines@users.noreply.github.com>
2025-03-16 17:33:30 +0000

    feature: Adding query string param.

--- src/http/handle_index.rs
+++ src/http/handle_index.rs
@@ -1,17 +1,26 @@
 use anyhow::Result;
-use axum::{extract::State, response::IntoResponse};
+use axum::{extract::{Query, State}, response::IntoResponse};
 use axum_template::RenderHtml;
 use minijinja::context as template_context;
+use serde::{Deserialize, Serialize};
 
 use crate::{errors::WeatherVaneError, http::context::WebContext};
 
+#[derive(Deserialize, Serialize)]
+pub struct FormHints {
+    pub query: Option<String>,
+}
+
 pub async fn handle_index(
     State(web_context): State<WebContext>,
+    Query(form_hints): Query<FormHints>,
 ) -> Result<impl IntoResponse, WeatherVaneError> {
     Ok(RenderHtml(
         "index.en-us.html",
         web_context.engine.clone(),
-        template_context! {},
+        template_context! {
+            subject_value => form_hints.query,
+        },
     )
     .into_response())
 }
